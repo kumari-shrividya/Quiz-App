ca740ba7d75b0d78310bf44f4c42395e65ac86a2
Kumari Shrividya Mol <90685998+kumari-shrividya@users.noreply.github.com>
2024-12-10 15:37:42 +0530

    Controller added

---
+++ controller/controller.js
@@ -0,0 +1,24 @@
+/**get all questions */
+export async function getQuestions(req, res){
+    res.json('questions api get request');
+}
+/** insert all questions */
+export async function insertQuestions(req, res){
+ res.json('questions api post request')
+}
+/** Delete questions */
+ export async  function deleteQuestions(req, res){
+    res.json('questions api  delete request')
+ }
+
+
+ /** get all result */
+
+ export async  function getResult(req, res){
+    res.json('result  api  get result')
+ }
+
+ /** post result */
+ export async  function postResult(req, res){
+    res.json('result  api  post result')
+ }
--- router/route.js
+++ router/route.js
@@ -1,12 +1,29 @@
 import { Router } from 'express';
 const router = Router();
 
+/**import controllers */
+import * as controller from '../controller/controller.js'
+
 /**Questions Routes API*/
-router.get('/questions', (req, res) => {
 
-    res.json('questions api get request');
+// router.get('/questions', (req, res) => {
 
-})
+//     res.json('questions api get request');
 
+// })
+
+// router.get('/questions', controller.getQuestions);
+// router.post('/questions',controller.insertQuestions);
+
+//alternative method to group all requests
+
+router.route('/questions')
+        .get(controller.getQuestions) /** GET request */
+        .post(controller.insertQuestions) /** POST request */
+        .delete(controller.deleteQuestions) /** DELETE request */
+
+router.route('/result')
+    .get(controller.getResult)
+    .post(controller.postResult)
  
 export default router;
