18503e968961a431fcfeca073b02e7b626a250e1
Kumari Shrividya Mol <90685998+kumari-shrividya@users.noreply.github.com>
2024-12-11 12:29:56 +0530

    Questions added to database

--- controller/controller.js
+++ controller/controller.js
@@ -1,19 +1,41 @@
+//import { response } from 'express';
+import Questions from '../models/questionSchema.js'
+import Results from '../models/resultSchema.js';
+import questions, { answers } from '../database/data.js'
+
 /**get all questions */
 export async function getQuestions(req, res){
-    res.json('questions api get request');
+   // res.json('questions api get request');
+   try{
+     const q = await Questions.find()
+     res.json(q)
+
+   } catch (error) {
+      res.json({ error })
+   }
 }
+
 /** insert all questions */
 export async function insertQuestions(req, res){
- res.json('questions api post request')
+ const docs = await	Questions.insertMany([{ questions, answers}])
+ res.json(docs)
+ //res.json('questions api post request')
 }
+
 /** Delete questions */
  export async  function deleteQuestions(req, res){
-    res.json('questions api  delete request')
+  //  res.json('questions api  delete request')
+  try{
+	await Questions.deleteMany();
+	res.json({ msg: 'Questions deleted'})
+
+  } catch (error){
+	res.json({ error })
+  }
  }
 
 
  /** get all result */
-
  export async  function getResult(req, res){
     res.json('result  api  get result')
  }
---
+++ database/data.js
@@ -0,0 +1,31 @@
+export default [
+    {
+        id:1,
+        question:"javascript is an ------------language",
+        options:[
+            'object-oriented',
+            'object-based',
+            'procedural'
+        ]
+    },
+    {
+    id:2,
+        question:"C++ is an ------------language",
+        options:[
+            'object-oriented',
+            'object-based',
+            'procedural'
+        ]
+    },
+    {
+        id:3,
+        question:"Java is an ------------language",
+        options:[
+            'object-oriented',
+            'object-based',
+            'procedural'
+        ]
+    }
+    ]
+    /**store all correct answers */
+    export const answers = [0, 0, 0]
